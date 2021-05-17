4ab4bfd6282448e1f39d076db2ac343911b82a59
Your Name <you@example.com>
2021-05-17 12:24:11 +0300

    hw11-1

---
+++ colorswitch.js
@@ -0,0 +1,36 @@
+const startEL = document.querySelector('[data-action = "start"]');
+const stopEl = document.querySelector('[data-action="stop"]');
+const NOTIFICATION_DELAY = 1000;
+let timerId = null;
+
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startEL.addEventListener('click', pressStart);
+stopEl.addEventListener('click', pressStop); 
+
+
+function pressStart() {
+   timerId =setInterval(() => {
+    document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, NOTIFICATION_DELAY);
+  startEL.disabled = true;
+};
+  
+function pressStop() {
+    clearInterval(timerId);
+  stopEl.disabled = false;
+  startEL.disabled = false;
+};
+
